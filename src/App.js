35e3714147c3af60953d22388d864a68f064d102
Alyssa Favorito <alyssafavorito2000@gmail.com>
2024-01-30 21:01:51 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Watchlist from './components/watchlist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL  = "";
+
+// edit App function to be a class and export
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addShow = this.addShow.bind(this);
+    this.deleteShow = this.deleteShow.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* map watchlist list into watchlist component*/}
+        {watchlistList.map((watchlist,index) => (
+          <Watchlist key = {index} watchlists = {watchlist} addShow = {this.addShow} deleteShow = {this.deleteShow}/>
+        ))}
+      </div>
+    );
+  }
+// HTTP requests
+// grab watchlists data
+  componentDidMount(){
+    fetch(URL)
+      .then (res => res.json())
+      .then (data => {
+        this.setState({
+        // might have to change to 'watchlist'
+        watchlists: data
+      });
+    });
+  }
+// delete and add show methods 
+  deleteShow(e, watchlist, show){
+    const index = watchlist.shows.indexOf(show);
+    watchlist.shows.splice(index, 1);
+    updateWatchlist(watchlist);
+  }
+
+  addShow(){
+
+  }
+
 }
 
-export default App;
+// update function to update watchlist after add or delete of show
+function updateWatchlist(watchlist) {
+  return fetch (`${URL}/${watchlist.id}`, 
+  {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(show)
+    });
+}
