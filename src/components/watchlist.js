233534f810395ce38c0ef4afc05d3845fd031745
Alyssa Favorito <alyssafavorito2000@gmail.com>
2024-02-08 17:08:47 -0500

    start of week 16

--- src/components/watchlist.js
+++ src/components/watchlist.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { ShowForm } from './showForm';
+import EditForm from './editForm';
 
 export default function Watchlist({ watchlist, updateWatchlist, deleteWatchlist }) {
     // const { watchlist, updateWatchlist, deleteWatchlist } = props;
 
+    // new feature: edit show
+    // const editShow = (showId, editName, editType, editService, editGenre) => {
+    //     const updatedWatchlist = {
+    //         ...watchlist, shows: [...watchlist.shows, {name: editName, type: editType, streamingService: editService, genre: editGenre, id: showId}]
+    //     }
+    //     // access current information and change it
+    //     // editable text or use add show form? new form?
+    //     this.updateWatchlist(updatedWatchlist);
+    //     return(
+    //         <div>
+    //             <EditForm />
+    //         </div>
+    //     )
+    // }
 
     const deleteShow = (showId) => {
         // creating an updated copy of watchlist which copies watchlist object and filters out the corresponding show by id from shows property within watchlist
@@ -30,7 +45,10 @@ export default function Watchlist({ watchlist, updateWatchlist, deleteWatchlist 
             {watchlist.shows.map((show, index) => (
                 <div key={index}>
                 <li className='list-group-item bg-dark border-dark text-light'>{`${show.name}`}
-                    <button className='btn btn-outline-warning ms-2' onClick={()=>deleteShow(show.id)}>
+                    {/* <button className='btn btn-outline-warning ms-2' onClick={()=>editShow(show.id)}>
+                        Edit Show
+                    </button> */}
+                    <button className='btn btn-outline-secondary ms-2' onClick={()=>deleteShow(show.id)}>
                         Delete Show
                     </button>
                 </li>
@@ -55,6 +73,7 @@ export default function Watchlist({ watchlist, updateWatchlist, deleteWatchlist 
             {renderShows()}
             {/* props to be used in Show Form component */}
             <ShowForm addShow={addShow} updateWatchlist = {updateWatchlist} />
+            {/* <EditForm editShow={editShow} updateWatchlist = {updateWatchlist} /> */}
         </div>
     )
 };
